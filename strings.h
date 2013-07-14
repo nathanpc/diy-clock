41585c15c348c7d5b77ccf959d0e7b24f8df24ff
Nathan Campos <eeepc904@gmail.com>
2013-07-14 00:54:44 -0300

    Added a header with some useful strings

---
+++ strings.h
@@ -0,0 +1,8 @@
+/**
+ *  strings.c
+ *  All the text and shit.
+ *
+ *  @author Nathan Campos <nathanpc@dreamintech.net>
+ */
+
+static const char month[][4] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
